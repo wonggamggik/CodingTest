83974ea28840d4e27b9a7d5a095eed9eaab84b59
wonjongin <wonggamggik@gmail.com>
2024-08-18 10:11:01 +0900

    Create 2743_단어 길이 재기_백준.js

---
+++ wonggamggik/Baekjoon 문제 풀이 코드/2743_단어 길이 재기_백준.js
@@ -0,0 +1,7 @@
+let input = require("fs").readFileSync("/dev/stdin").toString().trim();
+// const input = require("fs")
+//   .readFileSync("baekjoon_nodejs_2743.txt")
+//   .toString()
+//   .trim();
+
+console.log(input.length);
